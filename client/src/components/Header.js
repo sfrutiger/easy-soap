fc6569302a71a1153f37c140c48da9fe7a48c557
Seth Frutiger <sethfrutiger@gmail.com>
2022-04-24 19:58:39 -0400

    edited header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,7 +3,7 @@ const Header = () => {
     <header className="text-center mb-4">
       <h1 className="text-2xl sm:text-4xl">Easy SOAP</h1>
       <h2 className="border-b-2 text-xs sm:text-base">
-        A web based EMR that's easy to use and easy to look at
+        A web-based EMR system that's easy to use and easy to look at
       </h2>
     </header>
   );
