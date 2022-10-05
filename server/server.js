bc738442c4327a0cd9e0ac665fd3174d32e143b2
Seth Frutiger <sethfrutiger@gmail.com>
2022-10-04 20:43:02 -0400

    wake up dyno

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
+const wakeUpDyno = require("./wakeUpDyno.js");
 
 connectDB();
 
